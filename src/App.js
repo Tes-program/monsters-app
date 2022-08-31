476e731b8234f60816dd6b3f3274fe5c6b2b7aa2
Tes-program <teslimodumuyiwa@gmail.com>
2022-08-31 15:57:14 +0100

    component fully built

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'; 
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component';
 import './App.css';
 
 class App extends Component {
@@ -7,28 +7,19 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein',
-          id: 'tes1'
-        },
-        {
-          name: 'Dracula',
-          id: 'tes2'
-        },
-        {
-          name: 'Zombies',
-          id: 'tes3'
-        }
-      ]
+      monsters: []
     }
+  };
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({ monsters: users }))
   }
   render () {
     return (
       <div className='App'>
-        {
-          this.state.monsters.map( monster => <h1 key={monster.id}> { monster.name } </h1> )
-        }
+        <input type='search' placeholder='search monsters'/>
+        <CardList monsters = {this.state.monsters}/>
       </div>
     )
   }
